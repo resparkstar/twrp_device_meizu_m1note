f4cec6c133be93177afba241c4ffb70820a9d994
resparkstar <39150614+resparkstar@users.noreply.github.com>
2025-03-07 11:29:56 +0800

    Update omni_m1note.mk

--- omni_m1note.mk
+++ omni_m1note.mk
@@ -1,27 +1,38 @@
-PRODUCT_RELEASE_NAME := m1note
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit from the common Open Source product configuration
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit from M5Note device
+$(call inherit-product, device/meizu/m1note/device.mk)
 
-# Inherit from our custom product configuration
-
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-PRODUCT_PACKAGES += \
-
-charger_res_images \
-
-charger
-
-## Device identifier. This must come after all inclusions
-
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m1note
-
-PRODUCT_MODEL := m1note
-
 PRODUCT_NAME := omni_m1note
-
 PRODUCT_BRAND := meizu
-
+PRODUCT_MODEL := m1 note
 PRODUCT_MANUFACTURER := meizu
+PRODUCT_RELEASE_NAME := meizu m1 note
