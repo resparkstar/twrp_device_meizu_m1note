7fb8fae2fa6d83a0eef47c22c981149af990f6e1
resparkstar <39150614+resparkstar@users.noreply.github.com>
2025-03-15 07:41:06 +0800

    Add files via upload

--- omni_m1note.mk
+++ omni_m1note.mk
@@ -1,27 +1,29 @@
-PRODUCT_RELEASE_NAME := m1note
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-# Inherit from the common Open Source product configuration
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit from our custom product configuration
-
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_PACKAGES += \
-
-# charger_res_images \
-
-# charger
-
-## Device identifier. This must come after all inclusions
+# Inherit from m1note device
+$(call inherit-product, device/meizu/m1note/device.mk)
 
 PRODUCT_DEVICE := m1note
-
-PRODUCT_MODEL := m1note
-
 PRODUCT_NAME := omni_m1note
-
-PRODUCT_BRAND := meizu
-
+PRODUCT_BRAND := Meizu
+PRODUCT_MODEL := m1 note
 PRODUCT_MANUFACTURER := meizu
+
+PRODUCT_GMS_CLIENTID_BASE := android-meizu
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="meizu_m1note-user 5.1 LMY47D 1488769948 release-keys"
+
+BUILD_FINGERPRINT := Meizu/m1note/m1note:5.1/LMY47D/1488769948:user/release-keys
