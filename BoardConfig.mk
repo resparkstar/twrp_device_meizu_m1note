dd8eb2a68ab27175f86d85efaadb74b738828001
resparkstar <39150614+resparkstar@users.noreply.github.com>
2025-03-14 23:18:32 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,149 +1,86 @@
-LOCAL_PATH := device/meizu/m1note
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-#下面的复制相同cpu机型的，或者自己查手机cpu信息，m1note只能在安卓4.4解锁，所以是32位
+DEVICE_PATH := device/meizu/m1note
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := generic
 
-TARGET_ARCH := arm
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := generic
 
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := m1note
 TARGET_NO_BOOTLOADER := true
 
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# Kernel
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x03f88000
+BOARD_KERNEL_TAGS_OFFSET := 0x0df88000
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_CONFIG := m1note_defconfig
+TARGET_KERNEL_SOURCE := kernel/meizu/m1note
+
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+endif
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 12326912
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12326912
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Platform
 TARGET_BOARD_PLATFORM := mt6752
 
-TARGET_CPU_ABI := armeabi-v7a
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
-TARGET_CPU_ABI2 := armeabi
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-08-01
 
-TARGET_ARCH_VARIANT := armv7-a-neon
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
 
-TARGET_CPU_VARIANT := generic
-
-TARGET_CPU_CORTEX_A53 := true
-
-TARGET_CPU_SMP := true
-
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
-TARGET_NO_FACTORYIMAGE := true
-
-BOARD_MTK_LIBSENSORS_NAME :=
-
-BOARD_MTK_LIB_SENSOR :=
-
-#Bootloader
-
-TARGET_BOOTLOADER_BOARD_NAME := m1note
-
-#内核，用CarlivImageKitchen64解包官方boot或recovery就能得到这些
-
-#Kernel
-
-#内核路径
-
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-
-#mtk需要这个
-
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkmtkboot.mk
-
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32S1,32S1
-
-BOARD_KERNEL_BASE := 0x40078000
-
-BOARD_KERNEL_PAGESIZE := 2048
-
-BOARD_MKBOOTIMG_ARGS := \
-
---base 0x40000000 \
-
---pagesize 2048 \
-
---kernel_offset 0x00008000 \
-
---ramdisk_offset 0x04000000 \
-
---second_offset 0x00f00000 \
-
---tags_offset 0x0e000000 \
-
-#分区大小，adb shell,cat /proc/partinfo,分区size要从十六进制转10进制
-
-#Partition sizes
-
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
-
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 13217824768
-
-BOARD_CACHEIMAGE_PARTITION_SIZE := 117440512
-
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-
-#Recovery
-
-#编写好的recovery.fstab的路径
-
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
-
+# TWRP Configuration
 TW_THEME := portrait_hdpi
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-DEVICE_RESOLUTION := 1080x1920
-
-TW_NO_USB_STORAGE := true
-
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-
- 
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
-
-RECOVERY_FONT := roboto_15x24.h
-
-WITH_DEXPREOPT := true
-
-BOARD_HAS_FLIPPED_SCREEN := true
-
-TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
-
-# The path to a temperature sensor
-
-TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
-
-# USB Mounting
-
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/f_mass_storage/lun/file
-
-TW_MTP_DEVICE := /dev/mtp_usb
-
 TW_EXTRA_LANGUAGES := true
-
-#cp bootable/recovery/gui/theme/extra-languages/languages/zh_CN.xml bootable/recovery/gui/theme/common/languages/cn.xml
-
-TW_DEFAULT_LANGUAGE := cn
-
-# Storages
-
-BOARD_HAS_NO_MISC_PARTITION := true
-
-TW_INTERNAL_STORAGE_PATH := "/data/media"
-
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-
-TW_FLASH_FROM_STORAGE := true
-
-RECOVERY_SDCARD_ON_DATA := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
